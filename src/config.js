91338af160110e0bcff46351b0e4028684dc4bfc
manoj singh rawat <munnamanoj620@gmail.com>
2025-07-25 17:32:27 +0530

    add config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const CLERK_KEY = "pk_test_c2ltcGxlLWJlYXItNDguY2xlcmsuYWNjb3VudHMuZGV2JA";
+export const Api_key =  "AIzaSyDzut5WMV5VmxVnWX4wN08pnI8pJVs5MRk";
+export const Auth_Domain =  "youthiapa-project.firebaseapp.com";
+export const Project_Id = "youthiapa-project";
+export const Storage_Bucket = "youthiapa-project.appspot.com";
+export const Messaging_SenderId =  "211394445286";
+export const App_Id = "1:211394445286:web:5546826b9926ce4d6e2027" ;
